c6bd9ab4af3568a0f10dc1bc99f11ab654f60988
fengzhiken <fengzhiken@gmail.com>
2023-06-03 21:35:41 +0545

    `add`: added peek.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -90,6 +90,15 @@ local plugins = {
 			"nvim-tree/nvim-web-devicons",
 		},
 	},
+	{
+		"toppair/peek.nvim",
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup({})
+			vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+			vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+		end,
+	},
 }
 
 return plugins
