59952015f4c24084a318f0d9f5e52345c6a38e7c
fengzhiken <fengzhiken@gmail.com>
2023-06-27 20:02:31 +0545

    `update`: added more configs and setup script

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -71,6 +71,20 @@ local plugins = {
 		},
 	},
 	{
+		"sotte/presenting.vim",
+		config = function()
+			vim.cmd[[
+				augroup presentation
+					autocmd!
+					" Presentation mode
+					au Filetype markdown nnoremap <buffer> <F10> :PresentingStart<CR>
+					" ASCII art
+					au Filetype markdown nnoremap <buffer> <F12> :.!toilet -w 200 -f term -F border<CR>
+				augroup end
+			]]
+		end,
+	},
+	{
 		"lervag/vimtex",
 	},
 	{
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ~/dot/nvim ~/.config/nvim
+ln -s ~/dot/alacritty ~/.config/alacritty
+ln -s ~/dot/sway ~/.config/sway
+ln -s ~/dot/swaylock ~/.config/swaylock
+ln -s ~/dot/waybar ~/.config/waybar
+ln -s ~/dot/i3 ~/.config/i3
