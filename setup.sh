03328735c07f18a22e7d2621eb478e55399897d9
fengzhiken <fengzhiken@gmail.com>
2023-06-27 20:08:12 +0545

    `update`: remove folder before setting up

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+rm -rf ~/.config/nvim
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/sway
+rm -rf ~/.config/swaylock
+rm -rf ~/.config/waybar
+rm -rf ~/.config/i3
+
 ln -s ~/dot/nvim ~/.config/nvim
 ln -s ~/dot/alacritty ~/.config/alacritty
 ln -s ~/dot/sway ~/.config/sway
