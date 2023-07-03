450f573275dfe7ad92393fb032390471dda0f17a
fengzhiken <fengzhiken@gmail.com>
2023-07-03 16:25:17 +0545

    `setup.sh`: updated setup script

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ rm -rf ~/.config/sway
 rm -rf ~/.config/swaylock
 rm -rf ~/.config/waybar
 rm -rf ~/.config/i3
+rm -rf ~/Pictures/Wallpapers
+rm -rf ~/Scripts
 
 ln -s ~/dot/nvim ~/.config/nvim
 ln -s ~/dot/alacritty ~/.config/alacritty
@@ -13,3 +15,7 @@ ln -s ~/dot/sway ~/.config/sway
 ln -s ~/dot/swaylock ~/.config/swaylock
 ln -s ~/dot/waybar ~/.config/waybar
 ln -s ~/dot/i3 ~/.config/i3
+ln -s ~/dot/Scripts ~/
+
+mkdir Pictures
+ln -s ~/dot/Wallpapers ~/Pictures
